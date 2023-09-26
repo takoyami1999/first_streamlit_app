fd206c53178703fd8cbf60b81d87e7a02eab36ff
takoyami1999 <128781170+takoyami1999@users.noreply.github.com>
2023-09-26 15:21:44 +0800

    Update streamlit_app.py
    Added display of selected data

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,8 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Option to Pick Fruits
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #Data Visualization
 streamlit.dataframe(my_fruit_list)
