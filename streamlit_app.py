ecb6f7610313c088e8869527e4e81c02a79f88ed
takoyami1999 <128781170+takoyami1999@users.noreply.github.com>
2023-09-27 10:17:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,26 @@ import streamlit
 import pandas
 
 #Title of the Diner
-streamlit.title('👨🏿👨🏿👨🏿👨🏿👨🏿👨🏿👨🏿')
-streamlit.title('👨🏿👨🏿👨🏿👨🏿👨🏿👨🏿👨🏿')
-streamlit.title('👨🏿👨🏿👨🏿👨🏿👨🏿👨🏿👨🏿')
-streamlit.title('👨🏿👨🏿👨🏿👨🏿👨🏿👨🏿👨🏿')
-streamlit.title('👨🏿👨🏿👨🏿👨🏿👨🏿👨🏿👨🏿')
+streamlit.title('MY PARENTS NEW HEALTHY DINER!')
+
+#Menu
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+#Smoothie
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+#Data
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+#Option to Pick Fruits
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+#Data Visualization
+streamlit.dataframe(fruits_to_show)
+
