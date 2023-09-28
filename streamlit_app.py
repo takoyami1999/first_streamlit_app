9bfa2d106bbe84f4cd7581cefdd93dcf6328d6e4
takoyami1999 <128781170+takoyami1999@users.noreply.github.com>
2023-09-28 09:23:30 +0800

    Update streamlit_app.py
    additional exception handler try else

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,12 +35,19 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.header("Fruityvice Fruit Advice!")
 
 #input for fruit_choice
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information
+                    
+  else:
+  #normalize api response to a database
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-#normalize api response to a database
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 #stop
 streamlit.stop()
